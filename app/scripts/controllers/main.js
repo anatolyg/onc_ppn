81cbb8cc2490494ac4a89801247637d51e6a49ad
Anatoly Geyfman <anatoly@geyfman.net>
2014-04-07 10:58:45 -0700

    installed grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('oncPpnApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
