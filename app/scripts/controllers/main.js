0feba5f6190c11c43f43d9fd424230596829238c
Anatoly Geyfman <anatoly@geyfman.net>
2014-04-07 13:57:47 -0700

    list style for items, new directive to collapse/show

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/*global $:false */
 (function () {
 'use strict';
 
@@ -9,16 +10,32 @@ angular.module('oncPpnApp')
       'Karma'
     ];
   })
-    .directive('plMoreInfo', ['$rootScope', function ($rootScope) {
+    .directive('plMoreInfo', [function () {
         return {
-            link: function ($scope, $el, attrs) {
-                $el.find('[data-role=more-info]').on('click', function (e) {
+            link: function ($scope, $el) {
+                $el.find('[data-role=more-info]').on('click', function () {
                     var $contents = $el.find('[data-role=more-info-contents]');
                     $contents.toggle();
                     $(this).children('[data-role=toggle]')
                         .toggleClass('fa-chevron-circle-down', $contents.is(':visible'))
                         .toggleClass('fa-chevron-circle-right', !$contents.is(':visible'));
                 }).prepend('<i class="fa-chevron-circle-right" data-role="toggle"></i> ');
+            }
+        };
+    }])
+    .directive('ppnCollapsedList', [function () {
+        return {
+            link: function ($scope, $el) {
+                // loop through each list-group-item, hide the list-group-item-text and add an event handler to the
+                //  list-group-item-heading to collapse/show it.
+                $el.find('.list-group-item-text').hide();
+                $el.find('.list-group-item').each(function (i, el) {
+                    $(el).find('.list-group-item-heading').append(' <i class="fa-question-circle"></i> ');
+                }).click(function () {
+                    var $info = $(this),
+                        $infoText = $info.find('.list-group-item-text');
+                    $infoText.toggle();
+                });
             }
         };
     }])
@@ -43,7 +60,7 @@ angular.module('oncPpnApp')
                 },
                 effectiveDate: 1395181351047,
                 applicability: 'This notice applies to Sun Radiology only.',
-                privacyOfficer: 'HIPAA Compliance Dept., Phone: 623-344-5450, 13943 N. 91st Ave, Ste A-102, Peoria, AZ  85381',
+                privacyOfficer: 'HIPAA Compliance Dept., Phone: 623-344-5450, 13943 N. 91st Ave, Ste A-102, Peoria, AZ 85381',
                 signOff: 'By checking this box and clicking the button below, you agree to the terms described above'
             }
         };
